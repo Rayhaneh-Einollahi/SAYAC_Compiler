4c71d58c61f091c49281c752b210e5855366e3d7
Mahdis <mhdys5998@gmail.com>
2025-07-12 00:44:03 +0330

    Add function test case.

---
+++ tests/CodeGeneration/func.c
@@ -0,0 +1,28 @@
+void f0() {
+    int x = 1;
+    int y = x + 1;
+}
+
+void f1(int x) {
+    int y = x + 1;
+}
+
+int f2() {
+    int x = 1;
+    return x;
+}
+
+int f3(int x) {
+    int y = x + 1;
+    return y;
+}
+
+int main() {
+    int z = 20;
+    int w = 30;
+    int x = 10;
+    f0();
+    f1(x);
+    int y = f2();
+    int z = f3(y);
+}
