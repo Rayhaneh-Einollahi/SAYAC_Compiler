4625e8ffac83eb7132b6fe2f95041056e86dc9ff
rayhaneh einollahi <rayhaneheinollahi@gmail.com>
2025-07-21 21:09:21 +0330

    chore: cleanup simplelang class

--- src/SimpleLang.java
+++ src/SimpleLang.java
@@ -2,7 +2,6 @@ import main.ast.nodes.Program;
 import main.codeGenerator.CodeGenerator;
 import main.grammar.SimpleLangLexer;
 import main.grammar.SimpleLangParser;
-import main.symbolTable.SymbolTable;
 import main.visitor.*;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -11,136 +10,41 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Stack;
+
 
 public class SimpleLang {
-
-    public static String removeComments(String code) {
-        StringBuilder result = new StringBuilder();
-        boolean inBlockComment = false;
-        StringBuilder lineBuffer = new StringBuilder();
-
-        String[] lines = code.split("\\r?\\n");
-
-        for (String line : lines) {
-            if (!inBlockComment) lineBuffer.setLength(0);
-            int i = 0;
-            int n = line.length();
-
-            while (i < n) {
-                if (!inBlockComment && i + 1 < n && line.charAt(i) == '/' && line.charAt(i + 1) == '*') {
-                    inBlockComment = true;
-                    i += 2;
-                } else if (inBlockComment && i + 1 < n && line.charAt(i) == '*' && line.charAt(i + 1) == '/') {
-                    inBlockComment = false;
-                    i += 2;
-                } else if (!inBlockComment && i + 1 < n && line.charAt(i) == '/' && line.charAt(i + 1) == '/') {
-                    break; // Skip rest of the line
-                } else if (!inBlockComment) {
-                    lineBuffer.append(line.charAt(i));
-                    i++;
-                } else {
-                    i++;
-                }
-            }
-
-            if (!inBlockComment) {
-                result.append(lineBuffer.toString());
-            }
-            result.append("\n"); // Preserve original line breaks
-        }
-
-        return result.toString();
-    }
-    public static String preprocess(String code) {
-        String[] lines = code.split("\\R");
-        StringBuilder result = new StringBuilder();
-
-        Stack<Integer> indentStack = new Stack<>();
-        indentStack.push(-1);
-
-        for (String line : lines) {
-            String trimmed = line.stripLeading();
-            int indent = line.length() - trimmed.length();
-
-            while (!trimmed.isEmpty() && indent <= indentStack.peek()) {
-                result.append(" ".repeat(indentStack.pop())).append("}");
-            }
-
-            if (trimmed.equals("end")) {
-                result.append(" ".repeat(indent)).append("\n");
-            } else if (trimmed.endsWith(":")) {
-                result.append(" ".repeat(indent)).append(trimmed, 0, trimmed.length() - 1).append(" {\n");
-                indentStack.push(indent);
-            } else if (!trimmed.isEmpty()) {
-                result.append(" ".repeat(indent)).append(trimmed).append(";\n");
-            }
-            else{
-                result.append(" ".repeat(indent)).append(trimmed).append("\n");
-            }
-        }
-
-        while (indentStack.size() > 1) {
-            result.append(" ".repeat(indentStack.pop())).append("}\n");
-        }
-
-        return result.toString();
-    }
-
     public static void main(String[] args) throws IOException {
         String code = Files.readString(Paths.get(args[0]));
-//        String transformedCode = preprocess(removeComments(code));
-//        System.out.println(transformedCode);
         CharStream reader = CharStreams.fromString(code);
 
         SimpleLangLexer SimpleLangLexer = new SimpleLangLexer(reader);
         CommonTokenStream tokens = new CommonTokenStream(SimpleLangLexer);
         SimpleLangParser flParser = new SimpleLangParser(tokens);
         Program program = flParser.compilationUnit().programRet;
-        System.out.println();
 
-        boolean ok;
-//        while(true) {
-//            NameAnalyzer my_name_analyzer = new NameAnalyzer();
-//            my_name_analyzer.visit(program);
-//            if (!my_name_analyzer.ok) break;
-//
-//            UnusedRemover my_unusedRemover = new UnusedRemover();
-//            my_unusedRemover.visit(program);
-////            System.out.println("unused remover:");
-////            TestVisitor my2_visitor = new TestVisitor();
-////            my2_visitor.visit(program);
-//            if(!my_unusedRemover.ok) continue;
-//
-//            DeadStmtRemover my_deadRemover = new DeadStmtRemover();
-//            my_deadRemover.visit(program);
-////            System.out.println("dead remover:");
-////            TestVisitor my3_visitor = new TestVisitor();
-////            my3_visitor.visit(program);
-//            if(!my_deadRemover.ok) continue;
-//
-//            DefRemover my_defRemover = new DefRemover();
-//            my_defRemover.visit(program);
-////            System.out.println("def remover:");
-////            TestVisitor my4_visitor = new TestVisitor();
-////            my4_visitor.visit(program);
-//            if(!my_defRemover.ok) continue;
-//
-//            AccessAnalyzer my_AccessAnalyzer = new AccessAnalyzer();
-//            my_AccessAnalyzer.visit(program);
-//            if(!my_AccessAnalyzer.ok) continue;
-//
-//            TestVisitor my6_visitor = new TestVisitor();
-//            my6_visitor.visit(program);
-//            break;
-//
-//        }
+
+        NameAnalyzer name_analyzer = new NameAnalyzer();
+        name_analyzer.visit(program);
+
+        UnusedRemover unusedRemover = new UnusedRemover();
+        unusedRemover.visit(program);
+
+        DeadStmtRemover deadRemover = new DeadStmtRemover();
+        deadRemover.visit(program);
+
+        DefRemover defRemover = new DefRemover();
+        defRemover.visit(program);
+
+        AccessAnalyzer accessAnalyzer = new AccessAnalyzer();
+        accessAnalyzer.visit(program);
+
+        TestVisitor my6_visitor = new TestVisitor();
+        my6_visitor.visit(program);
+
 
         CodeGenerator codeGenerator = new CodeGenerator();
         codeGenerator.visit(program);
         codeGenerator.printAssembly();
 
-//        SymbolTable.root.hashCode();
-//        System.out.println();
     }
 }
