11ebf07471e25421530abeacde69c272c2fd84ce
rayspring2 <bahareheinollahi84@gmail.com>
2025-10-06 00:26:06 +0330

    test: Add new array-ifelse test

---
+++ tests/CodeGeneration/If_Else/if-else_03.c
@@ -0,0 +1,18 @@
+int main(){
+    int a = 1;
+    int b = 1;
+    if(a > 1){
+        b = 2;
+    }
+    else if(a == 0) {
+        b = 10;
+    }
+    else{
+        b = 3;
+        if(b > 2){
+            b +=1;
+        }
+    }
+
+    return b;
+}
