de7eb93033352a01f8082912f35afd273ae878ab
rayhaneh.einollahi <rayhaneheinollahi@gmail.com>
2025-10-05 16:34:58 +0330

    feat(utility): Add benchmarks to test directories

--- utilities/CreateConfigs.py
+++ utilities/CreateConfigs.py
@@ -1,13 +1,15 @@
 import os
+import itertools
 
 tests_dir = "tests"
+bench_tests_dir = "../C_Integer_Benchmarks"
 output_dir = ".idea/runConfigurations"
 module_name = "SAYAC_Compiler"  # change if needed
 main_class = "SimpleLang"       # change if needed
 
 os.makedirs(output_dir, exist_ok=True)
 
-for root, _, files in os.walk(tests_dir):
+for root, _, files in itertools.chain(os.walk(tests_dir), os.walk(bench_tests_dir)):
     for file in files:
         if file.endswith(".c"):
             rel_path = os.path.join(root, file).replace("\\", "/")
@@ -17,7 +19,7 @@ for root, _, files in os.walk(tests_dir):
   <configuration name="{config_name}" type="Application" factoryName="Application">
     <option name="MAIN_CLASS_NAME" value="{main_class}" />
     <module name="{module_name}" />
-    <option name="PROGRAM_PARAMETERS" value="{rel_path}" />
+    <option name="PROGRAM_PARAMETERS" value="&quot;{rel_path}&quot;" />
     <method v="2">
       <option name="Make" enabled="true" />
     </method>
